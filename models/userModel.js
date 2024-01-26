2133f6625a2f9a04e29cbc0561ea9b21223e4194
Fernando José <fernando.tecnicoinfo7@gmail.com>
2024-01-26 10:31:06 -0300

    Removing unnecessary model user id

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,5 @@
 class User {
-    constructor(id, first_name, last_name, email, password, avatar) {
-        this.id = id;
+    constructor(first_name, last_name, email, password, avatar) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.email = email;
