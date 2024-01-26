1a7e51e60de2a5be9205adeefb459b19a7feba18
Fernando José <fernando.tecnicoinfo7@gmail.com>
2024-01-26 10:33:59 -0300

    Changing `getUsers` and `addUser` functions to reuse the database with Prisma

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,33 @@
 import User from "../models/userModel.js";
+import { z } from "zod";
 
-const users = [];
+import { PrismaClient } from "@prisma/client";
 
-export function getUsers(req, res) {
+const PRISMA = new PrismaClient();
+
+export async function getUsers(req, res) {
+    let users = await PRISMA.users.findMany();
+
     res.json(users);
 }
-export function addUser(req, res) {
-    const { name, email } = req.body;
-    const newUser = new User(Date.now(), name, email);
-    users.push(newUser);
-    res.status(201).json(newUser);
+
+export async function addUser(req, res) {
+    //validação dos dados com ZOD
+    const userSchema = z.object({
+        first_name: z.string(),
+        last_name: z.string(),
+        email: z.string(),
+        password: z.string(),
+        avatar: z.string(),
+    });
+    let body = userSchema.safeParse(req.body);
+    console.log(body);
+    if (body.success) {
+        let user = await PRISMA.users.create({ data: body.data });
+
+        res.status(201).json(user);
+    } else {
+        // melhorar saída do erro
+        res.status(400).json({ Erro: "Dados Inválidos!" });
+    }
 }
