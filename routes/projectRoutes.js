d87fb941b2fdc965f644cd3e26d1b6faf4c9e6b5
Tiago Lopes <tiagoltavares2002@gmail.com>
2024-01-30 21:56:42 -0300

    feat: create route GET /users/{user_id}/projects/{id}
    - created a separated file for project schema
    - refactoring using error classes from the api-errors.js file

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
     create,
     getAllUserProjects,
+    getUserProjectById,
 } from "../controllers/projectController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.post("/users/:id/projects", authMiddleware, create);
 router.get("/users/:id/projects", authMiddleware, getAllUserProjects);
+router.get("/users/:user_id/projects/:id", authMiddleware, getUserProjectById);
 
 export default router;
