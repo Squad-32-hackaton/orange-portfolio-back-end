fdd7fb126605484d9f81108cba48d1febc6d2cb1
Fernando José <fernando.tecnicoinfo7@gmail.com>
2024-02-01 15:55:05 -0300

    refactor(user): add treatment with type scheme for maximum characters in user entity input fields

--- zodSchemas/userSchema.js
+++ zodSchemas/userSchema.js
@@ -1,10 +1,31 @@
 import { z } from "zod";
 //validação dos dados com ZOD
 const userSchema = z.object({
-    first_name: z.string(),
-    last_name: z.string(),
-    email: z.string(),
-    password: z.string(),
+    first_name: z
+        .string({
+            required_error: "Field 'first_name' is required",
+            invalid_type_error: "Field 'name' must be a string",
+        })
+        .max(50),
+    last_name: z
+        .string({
+            required_error: "Field 'last_name' is required",
+            invalid_type_error: "Field 'last_name' must be a string",
+        })
+        .max(50),
+    email: z
+        .string({
+            required_error: "Field 'email' is required",
+            invalid_type_error: "Field 'email' must be a string",
+        })
+        .email({
+            invalid_type_error: "This is not a valid email",
+        })
+        .max(50),
+    password: z.string({
+        required_error: "Field 'password' is required",
+        invalid_type_error: "Field 'password' must be a string",
+    }),
     avatar_id: z.number().optional(),
 });
 export default userSchema;
