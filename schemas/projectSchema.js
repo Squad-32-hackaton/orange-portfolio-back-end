2ce02a9ba84a2461a59cd150b50aa51b05628f27
Tiago Lopes <tiagoltavares2002@gmail.com>
2024-02-03 19:03:25 -0300

    refactor: add new validations for project fields
    - add validation for maximum and minimum size
    - add validation for blank spaces

--- schemas/projectSchema.js
+++ schemas/projectSchema.js
@@ -1,23 +1,41 @@
 import { z } from "zod";
 
 export const createAndUpdate = z.object({
-    title: z.string({
-        required_error: "Field 'title' is required",
-        invalid_type_error: "Field 'title' must be a string",
-    }),
-    description: z.string({
-        required_error: "Field 'description' is required",
-        invalid_type_error: "Field 'description' must be a string",
-    }),
-    link: z.string({
-        required_error: "Field 'link' is required",
-        invalid_type_error: "Field 'link' must be a string",
-    }),
+    title: z
+        .string({
+            required_error: "Field 'title' is required",
+            invalid_type_error: "Field 'title' must be a string",
+        })
+        .trim()
+        .min(3, "Field 'title' should contain at least 3 characters")
+        .max(100, "Field 'title' should contain a maximum of 100 characters"),
+    description: z
+        .string({
+            required_error: "Field 'description' is required",
+            invalid_type_error: "Field 'description' must be a string",
+        })
+        .trim()
+        .min(3, "Field 'description' should contain at least 3 characters")
+        .max(
+            191,
+            "Field 'description' should contain a maximum of 191 characters",
+        ),
+    link: z
+        .string({
+            required_error: "Field 'link' is required",
+            invalid_type_error: "Field 'link' must be a string",
+        })
+        .trim()
+        .min(3, "Field 'link' should contain at least 3 characters")
+        .max(191, "Field 'link' should contain a maximum of 191 characters"),
     tags: z
         .string({
             required_error: "Field 'tags' is required",
             invalid_type_error: "Field 'tags' must be a string array",
         })
+        .trim()
+        .min(2, "Each tag should contain at least 2 characters")
+        .max(50, "Each tag should contain a maximum of 50 characters")
         .array(),
     image_id: z.number({
         required_error: "Field 'image_id' is required",
