51f5d1c4f6099ab2fc36e311b8ae6fd68afdc712
Tiago Lopes <tiagoltavares2002@gmail.com>
2024-02-03 18:23:30 -0300

    fix: adding treatment for when the token is invalid or expired

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -12,17 +12,21 @@ export async function authMiddleware(req, _, next) {
     // retira a string "baren" token da string
     const token = authorization.split(" ")[1];
 
-    const { id: user_id } = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+        const { id: user_id } = jwt.verify(token, process.env.JWT_SECRET);
 
-    // consulta no banco se o email existe
-    const user = await prisma.users.findFirst({ where: { user_id } });
+        // consulta no banco se o email existe
+        const user = await prisma.users.findFirst({ where: { user_id } });
 
-    if (!user) {
-        throw new UnauthorizedError("Invalid email or password!");
+        if (!user) {
+            throw new UnauthorizedError("Invalid email or password!");
+        }
+
+        const { password, createdAt, updatedAt, ...loggedUser } = user;
+        req.user = loggedUser;
+
+        next();
+    } catch (error) {
+        throw new UnauthorizedError("Invalid token!");
     }
-
-    const { password, createdAt, updatedAt, ...loggedUser } = user;
-    req.user = loggedUser;
-
-    next();
 }
