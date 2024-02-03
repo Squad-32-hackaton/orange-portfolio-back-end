39d976ce691d2c852de42d3c47c90c91da8abd6e
Tiago Lopes <tiagoltavares2002@gmail.com>
2024-02-03 17:58:19 -0300

    refactor: creating index file to facilitate import of routes

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+import loginRoutes from "./loginRoutes.js";
+import projectRoutes from "./projectRoutes.js";
+import uploadRoutes from "./uploadRoutes.js";
+import userRoutes from "./userRoutes.js";
+
+export { loginRoutes, projectRoutes, uploadRoutes, userRoutes };
